98c191248e08c8556a999e52316df0a99fd90562
Vera Schild <veraschild@gmail.com>
2018-09-04 21:56:05 +0200

    Checks for break or lists in module 1

--- tests/getaltheorie/reeksTest.py
+++ tests/getaltheorie/reeksTest.py
@@ -2,6 +2,16 @@ import checkpy.tests as t
 import checkpy.lib as lib
 import checkpy.assertlib as assertlib
 
+import os
+import sys
+
+curPath = os.path.realpath(__file__)
+fileName = os.path.basename(__file__)
+folderName = "getaltheorie"
+sys.path.append(curPath[:-(len(fileName) + len(folderName) + 1)])
+
+from notAllowedCode import *
+
 @t.test(0)
 def correctBarriers(test):
 	def testMethod():
@@ -10,11 +20,13 @@ def correctBarriers(test):
 		return testResult
 	test.test = testMethod
 
+	notAllowed = {"break": "break"}
+	notAllowedCode(test, lib.source(_fileName), notAllowed)
+
 	test.description = lambda : "geeft het correcte beginpunt en eindpunt van de reeks"
 	test.fail = lambda info : "let op: de priemgetallen horen niet bij de reeks!"
 
 @t.test(10)
 def correctDistance(test):
 	test.test = lambda : assertlib.numberOnLine(35, lib.getLine(lib.outputOf(_fileName), 1))
-	test.fail = lambda info : "print de correcte lengte van de reeks op de tweede regel"
 	test.description = lambda : "geeft de correcte lengte van de reeks"
--- tests/integraal/randomwiskundeTest.py
+++ tests/integraal/randomwiskundeTest.py
@@ -7,7 +7,6 @@ import math
 def correctAmount(test):
 	test.test = lambda : assertlib.numberOnLine(math.e, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.0027)
 	test.description = lambda : "Geeft de correcte gemiddeld aantal worpen"
-	test.succes = lambda info : "komt dit getal bekend voor?"
 
 
 	
