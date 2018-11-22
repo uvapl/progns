a39c8b9e1c6940f677af0689d3898cad1d459b70
Martijn Stegeman <martijn@stgm.nl>
2018-11-22 10:43:09 +0100

    tests re-enabled

--- tests/integreren/nulpuntenTest.py
+++ tests/integreren/nulpuntenTest.py
@@ -22,25 +22,25 @@ def after():
 		pass
 
 
-# @t.test(0)
-# def hasNulpunten(test):
-#     test.test = lambda : assertlib.fileContainsFunctionDefinitions(_fileName, "nulpunten")
-#     test.description = lambda : "definieert de functie nulpunten()"
-#
-# @t.passed(hasNulpunten)
-# @t.test(10)
-# def returnTypeIsList(test):
-#     test.test = lambda : assertlib.sameType(lib.getFunction("nulpunten", _fileName)(1,2,-10), [])
-#     test.description = lambda : "nulpunten geeft een lijst terug"
+@t.test(0)
+def hasNulpunten(test):
+    test.test = lambda : assertlib.fileContainsFunctionDefinitions(_fileName, "nulpunten")
+    test.description = lambda : "definieert de functie nulpunten()"
 
-# @t.passed(hasNulpunten)
+@t.passed(hasNulpunten)
+@t.test(10)
+def returnTypeIsList(test):
+    test.test = lambda : assertlib.sameType(lib.getFunction("nulpunten", _fileName)(1,2,-10), [])
+    test.description = lambda : "nulpunten geeft een lijst terug"
+
+@t.passed(hasNulpunten)
 @t.test(20)
 def correct(test):
 	test.test = lambda : assertlib.exact(sorted(int(p * 10) for p in lib.getFunction("nulpunten", _fileName)(1,2,-10)), [-43, 23])
-	test.description = lambda : "vind de nulpunten voor invoer a=1, b=2, c=-10"
+	test.description = lambda : "vindt de nulpunten voor invoer a=1, b=2, c=-10"
 
-# @t.passed(hasNulpunten)
-# @t.test(30)
-# def correctNone(test):
-#     test.test = lambda : assertlib.exact(sorted(int(p * 10) for p in lib.getFunction("nulpunten", _fileName)(3,6,9)), [])
-#     test.description = lambda : "vind geen nulpunten voor invoer a=3, b=6, c=9"
+@t.passed(hasNulpunten)
+@t.test(30)
+def correctNone(test):
+    test.test = lambda : assertlib.exact(sorted(int(p * 10) for p in lib.getFunction("nulpunten", _fileName)(3,6,9)), [])
+    test.description = lambda : "vindt geen nulpunten voor invoer a=3, b=6, c=9"
