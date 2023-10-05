a8a1b67370362dcd2b6adb215d7eb6cec3f84035
Martijn Stegeman <martijn@stgm.nl>
2023-10-05 08:59:38 +0200

    msgs

--- tests/integreren/plotTest.py
+++ tests/integreren/plotTest.py
@@ -44,5 +44,5 @@ def givesMin(test):
 	notAllowedCode(test, lib.source(_fileName), notAllowed)
 
 	test.test = lambda : assertlib.numberOnLine(0.37, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.05) and assertlib.numberOnLine(0.69, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.05)
-	test.fail = lambda info : "Zorg dat het juiste minimum op de eerste regel geprint wordt"
+	test.fail = lambda info : "zorg dat het juiste minimum op de eerste regel geprint wordt"
 	test.description = lambda : "print het juiste minimum van de functies"
--- tests/integreren/randomwiskundeTest.py
+++ tests/integreren/randomwiskundeTest.py
@@ -6,5 +6,5 @@ import math
 @t.test(0)
 def correctAmount(test):
 	test.test = lambda : assertlib.numberOnLine(math.e, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.0027)
-	test.description = lambda : "Geeft de correcte gemiddeld aantal worpen"
+	test.description = lambda : "geeft het correcte gemiddelde aantal worpen"
 	test.succes = lambda info : "komt dit getal bekend voor?"
